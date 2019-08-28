cc9c32468ee238b527f471b5f8babd98f54684cc
Luc Somers <lucwsomers@gmail.com>
2019-08-27 22:24:11 -0300

    step 2

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from pandas import DataFrame
-import statsmodels.api as sm
-
-
 Stock_Market = {'Year':
                 [2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016],
                 'Month': [12,
@@ -11,15 +8,7 @@ Stock_Market = {'Year':
                 'Unemployment_Rate':
                 [5.3, 5.3, 5.3, 5.3, 5.4, 5.6, 5.5, 5.5, 5.5, 5.6, 5.7, 5.9, 6, 5.9, 5.8, 6.1, 6.2, 6.1, 6.1, 6.1, 5.9, 6.2, 6.2, 6.1],
                 'Stock_Index_Price':
-                [1464, 1394, 1357, 1293, 1256, 1254, 1234, 1195, 1159, 1167, 1130,
-                 1075, 1047, 965, 943, 958, 971, 949, 884, 866, 876, 822, 704, 719]
+                [1464, 1394, 1357, 1293, 1256, 1254, 1234, 1195, 1159, 1167, 1130, 1075, 1047, 965, 943, 958, 971, 949, 884, 866, 876, 822, 704, 719]
                 }
-
 df = DataFrame(Stock_Market, columns=['Year', 'Month', 'Interest_Rate', 'Unemployment_Rate','Stock_Index_Price'])
-X = df[['Interest_Rate', 'Unemployment_Rate']]
-Y = df['Stock_Index_Price']
-X = sm.add_constant(X)
-model = sm.OLS(Y, X).fit()
-predictions = model.predict(X)
-print_model = model.summary()
-print(print_model)
+print(df)
