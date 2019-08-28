cf4443be3c09395c284c56cd45c32267e1cc5db8
Luc Somers <lucwsomers@gmail.com>
2019-08-27 22:46:15 -0300

    hello world

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM python:3.7
+FROM python:3-alpine
 COPY . /app
 WORKDIR /app
-RUN pip install -r requirements.txt
-CMD ["python", "./app.py"]
+RUN pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r requirements.txt
+ENTRYPOINT ["python"]
+CMD ["app.py"]
+EXPOSE 5000
--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-from pandas import DataFrame
-import statsmodels.api as sm
+from flask import Flask, request, make_response, jsonify
 
+app = Flask(__name__)
 
-Stock_Market = {'Year':
-                [2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016],
-                'Month': [12,
-                          11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-                'Interest_Rate':
-                [2.75, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.25, 2.25, 2.25, 2, 2, 2, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75],
-                'Unemployment_Rate':
-                [5.3, 5.3, 5.3, 5.3, 5.4, 5.6, 5.5, 5.5, 5.5, 5.6, 5.7, 5.9, 6, 5.9, 5.8, 6.1, 6.2, 6.1, 6.1, 6.1, 5.9, 6.2, 6.2, 6.1],
-                'Stock_Index_Price':
-                [1464, 1394, 1357, 1293, 1256, 1254, 1234, 1195, 1159, 1167, 1130,
-                 1075, 1047, 965, 943, 958, 971, 949, 884, 866, 876, 822, 704, 719]
-                }
+@app.route('/')
+def index():
+    return 'Hello World!'
 
-df = DataFrame(Stock_Market, columns=['Year', 'Month', 'Interest_Rate', 'Unemployment_Rate','Stock_Index_Price'])
-X = df[['Interest_Rate', 'Unemployment_Rate']]
-Y = df['Stock_Index_Price']
-X = sm.add_constant(X)
-model = sm.OLS(Y, X).fit()
-predictions = model.predict(X)
-print_model = model.summary()
-print(print_model)
+def results():
+    # req = request.get_json(force = True)
+    # action = req.get('queryResult').get('action')
+    return {'fulfillmentText': 'This is a response from webhook'}
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    return make_response(jsonify(results()))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
