c10e7431bfa6a946982670353913808ee5735d7a
Luc Somers <lucwsomers@gmail.com>
2019-08-27 22:32:46 -0300

    step 3

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from pandas import DataFrame
-Stock_Market = {'Year':
-                [2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016],
-                'Month': [12,
-                          11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-                'Interest_Rate':
-                [2.75, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.25, 2.25, 2.25, 2, 2, 2, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75],
-                'Unemployment_Rate':
-                [5.3, 5.3, 5.3, 5.3, 5.4, 5.6, 5.5, 5.5, 5.5, 5.6, 5.7, 5.9, 6, 5.9, 5.8, 6.1, 6.2, 6.1, 6.1, 6.1, 5.9, 6.2, 6.2, 6.1],
-                'Stock_Index_Price':
-                [1464, 1394, 1357, 1293, 1256, 1254, 1234, 1195, 1159, 1167, 1130, 1075, 1047, 965, 943, 958, 971, 949, 884, 866, 876, 822, 704, 719]
-                }
-df = DataFrame(Stock_Market, columns=['Year', 'Month', 'Interest_Rate', 'Unemployment_Rate','Stock_Index_Price'])
-print(df)
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+candidates = {'gmat':
+              [780, 750, 690, 710, 680, 730, 690, 720, 740, 690, 610, 690, 710, 680, 770, 610, 580, 650, 540,
+               590, 620, 600, 550, 550, 570, 670, 660, 580, 650, 660, 640, 620, 660, 660, 680, 650, 670, 580, 590, 690],
+              'gpa':
+              [4, 3.9, 3.3, 3.7, 3.9, 3.7, 2.3, 3.3, 3.3, 1.7, 2.7, 3.7, 3.7, 3.3, 3.3, 3, 2.7, 3.7, 2.7, 2.3,
+               3.3, 2, 2.3, 2.7, 3, 3.3, 3.7, 2.3, 3.7, 3.3, 3, 2.7, 4, 3.3, 3.3, 2.3, 2.7, 3.3, 1.7, 3.7],
+              'work_experience':
+              [3, 4, 3, 5, 4, 6, 1, 4, 5, 1, 3, 5, 6, 4, 3, 1, 4, 6, 2, 3, 2, 1, 4, 1, 2, 6, 4, 2, 6, 5, 1, 2, 4, 6, 5, 1, 2, 1,
+                  4, 5],
+              'admitted':
+              [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0,
+                  0, 1]
+              }
+df = pd.DataFrame(candidates, columns=['gmat',
+                                       'gpa', 'work_experience', 'admitted'])
+X = df[['gmat', 'gpa', 'work_experience']]
+y = df['admitted']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)  # train is based on 75% of the dataset, test is based on 25 % of dataset
+logistic_regression = LogisticRegression()
+logistic_regression.fit(X_train, y_train)
+y_pred = logistic_regression.predict(X_test)
+print(X_test)  # test dataset (without the actual outcome)
+print(y_pred)  # predicted values
